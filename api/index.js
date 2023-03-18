12ae1f42f4d06a20b141ce58b0e123e8a5676380
Diego Fernandez <diegoafanador10@gmail.com>
2023-03-17 22:31:39 -0400

    deploy vercel

--- api/index.js
+++ api/index.js
@@ -4,11 +4,11 @@ const cors = require('cors');
 const routerApi = require('./routes')
 const app = express()
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.use(cors)
+app.use(cors())
 
 app.get("/", (req,res)=>{
   res.send("Hola mundo en express")
